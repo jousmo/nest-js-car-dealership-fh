e0f47ae1271aabf5b3e7db5796aec9a6237e6f30
José Uscanga <jousmo@outlook.com>
2022-10-20 07:25:10 -0500

    feat: update dto for car using mapped types

--- src/cars/dto/update.car.dto.ts
+++ src/cars/dto/update.car.dto.ts
@@ -1,16 +1,4 @@
-import { IsOptional, IsString, IsUUID } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCarDto } from './create-car.dto';
 
-export class UpdateCarDto {
-  @IsString()
-  @IsUUID()
-  @IsOptional()
-  readonly id?: string;
-
-  @IsString()
-  @IsOptional()
-  readonly brand?: string;
-
-  @IsString()
-  @IsOptional()
-  readonly model?: string;
-}
+export class UpdateCarDto extends PartialType(CreateCarDto) {}
